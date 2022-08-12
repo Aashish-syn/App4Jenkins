1069fecafee9f222ae1d1e3f1e4c9ef388a9ff0a
aashish-syn <firedetection@gmail.com>
2022-08-12 15:40:56 +0530

    Enter new method

--- HelloApp/Program.cs
+++ HelloApp/Program.cs
@@ -12,8 +12,9 @@ namespace HelloApp
 
             Calc calc = new Calc();
 
-            Console.WriteLine($"Sum = {calc.Add(70, 80)}");
-            Console.WriteLine($"Multiply = {calc.Multiply(4, 5)}");
+            Console.WriteLine($"Sum = {calc.add(70, 80)}");
+            Console.WriteLine($"Subtract = {calc.multiply(4, 5)}");
+            Console.WriteLine($"Multiply = {calc.subtract(10,5)}");
             Console.ReadLine();
         }
     }
